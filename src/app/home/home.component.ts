e7dd1cd98131ad953dc2bf4acfc63570594fdec7
Cameron Jensen <jensenseanc@gmail.com>
2017-09-11 15:04:26 -0700

    add gotodetail method

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Chicken Stuffed", "Seared Chicken", "chicken, salt, pepper", "cook on stove top, add spices, heat to desired temperature", "this is best cooked at high heat then low heat", "Whole 30", "www.usfutbolnow.us", "dinner", 0),
+    new Recipe("Lasagna", "Fresh Lasagna", "noodles, sauce, meat, cheese", "soften noodles, add in layers, bake for a while", "low heat for a long amout of time to perfection", "Carb loading", "www.food.com", "dinner", 1)
+  ];
+
+  goToDetailPage(clickedRecipe: Recipe) {
+     this.router.navigate(['recipes', clickedRecipe.id]);
+   };
 
   constructor() { }
 
