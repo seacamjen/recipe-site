9feef4c0aece028895dff955f0c32face6e7a9c5
Cameron Jensen <jensenseanc@gmail.com>
2017-09-15 21:32:06 -0700

    removed mock recipes, removed title from app component

--- src/app/mock-recipes.ts
+++
@@ -1,6 +0,0 @@
-// import { Recipe } from './recipe.model';
-//
-// export const RECIPES: Recipe[] = [
-//   new Recipe("Chicken Stuffed", "Seared Chicken", "chicken, salt, pepper", "cook on stove top, add spices, heat to desired temperature", "this is best cooked at high heat then low heat", "Whole 30", "www.usfutbolnow.us", "dinner", 0),
-//   new Recipe("Lasagna", "Fresh Lasagna", "noodles, sauce, meat, cheese", "soften noodles, add in layers, bake for a while", "low heat for a long amout of time to perfection", "Carb loading", "www.food.com", "dinner", 1)
-// ];
